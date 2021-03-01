ea81fc2f620ba14abbdf757a6ecda916799a1757
Aaron Resch <aaronmresch@gmail.com>
2021-03-01 14:34:31 -0600

    phase 1 complete

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+Main();
+
+void Main()
+{
+  Console.WriteLine("Guess the secret number!");
+  string userInput = Console.ReadLine();
+  int userGuess = 0;
+  try
+  {
+    userGuess = Int32.Parse(userInput);
+  }
+  catch
+  {
+    Console.WriteLine("Nah, that didn't work. Try with a normal number, weirdo...");
+    Thread.Sleep(2000);
+    Console.Clear();
+    Main();
+  }
+  Console.WriteLine($"You guessed {userGuess}");
+}
